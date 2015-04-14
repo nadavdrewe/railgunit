d56858a92f94edc8c85a408a0a0bc413ffb497b3
Nadav Drewe <emailnadz@gmail.com>
2015-04-14 13:01:27 +0100

    Addded testimonial model

---
+++ web.railgun.com/Models/Testimonial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace web.railgun.com.Models
+{
+    public class Testimonial
+    {
+        [Key]
+        public int TestimonialId { get; set; }
+        public string Name { get; set; }
+        public int StarRating { get; set; }
+        public string ImageURL { get; set; }
+        public string Text { get; set; }
+
+    }
+}
