7f3540570e2dcea4968d1e5d5f833766c9fa870c
Nadav Drewe <emailnadz@gmail.com>
2015-04-16 15:50:14 +0100

    Added feature, pricing and piricngtier to model

---
+++ web.railgun.com/Models/Feature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace web.railgun.com.Models
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        
+        public string Name {get;set;}
+
+
+    }
+
+
+}
---
+++ web.railgun.com/Models/PricingTier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace web.railgun.com.Models
+{
+    public class PricingTier
+    {
+
+        [Key]
+
+        public int PricingTierId { get; set; }
+        public string TierName { get; set; }
+
+        public virtual ICollection<Pricing> Pricings { get; set; }
+
+
+    }
+}
